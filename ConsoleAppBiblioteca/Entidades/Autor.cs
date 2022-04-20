ceb6a65cd36ad6e10302954576b6e2c6b874ba1b
Mauro Luis Paim Martins <mpmartins@bol.com.br>
2022-04-20 14:21:05 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppBiblioteca/Entidades/Autor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBiblioteca.Entidades
+{
+    public class Autor
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public Livro Livros { get; set; }
+        public Autor(string nome,string sobrenome,Livro livros)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;  
+            Livros = livros;
+
+        } 
+    }
+
+}
---
+++ ConsoleAppBiblioteca/Entidades/Livro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBiblioteca.Entidades
+{
+    public class Livro
+    {
+        public int Codigo { get; set; }
+        public int CodigoCategoria { get; set; }
+        public int CodigoAutor { get; set; }
+        public string Titulo { get; set; }
+        public int AnoPublicacao { get; set; }
+        public Categoria Categoria { get; set; }
+        public Autor Autor { get; set; }
+         
+    }
+}
