602379ea576f7f6af55b71f2926efd6bb0d84cf2
Mauro Luis Paim Martins <mpmartins@bol.com.br>
2022-05-07 10:56:55 -0300

    ex10
    dddd

---
+++ Biblioteca.Api/Controllers/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Biblioteca.Api.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.Reflection;
+
+namespace Biblioteca.Api.Controllers
+{
+    public class DataContext : DbContext
+    {
+        //public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
